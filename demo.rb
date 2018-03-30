3863ce77a84a92dd71a6a579fedd3661568b60b3
Lysiek <lysiekchoun168@gmail.com>
2018-03-30 11:08:12 +0700

    Third change

--- demo.rb
+++ demo.rb
@@ -62,3 +62,16 @@ def my_string
 	
 end
 puts my_string
+
+# => Exercise 6: Write a method called called convert that takes one argument which is a temperature in degrees Fahrenheit.
+# => This method should return the temperature in degrees Celsius.
+# => To format the output to say 2 decimal places, we can use the Kernel's format method.
+# => For example if x = 45.5678 then format("%2f",x) will return the string "45.57". This is also to say that if x = 10 then
+# => format("$.2f") will return the string "10.00"
+# => Another way is to use the round funtion as follows: puts (x*100).round/100.0
+
+def convert(temperature_in_F)
+	(temperature_in_F - 32) / 1.8
+end
+
+puts "%.2" % convert(-40)
